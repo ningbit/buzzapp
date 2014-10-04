69b349d9af18653877f858816c9461fe12d7e0e4
HUGE | Ning Yap <nyap@hugeinc.com>
2014-10-04 17:21:15 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuzzApp::Application.config.secret_token = '4f99e3c7874d463e222e286dd46fc1fcb523f003955dbf2a3897f5083a3ac1670512a993a30f6651ec167dff3b5b80b41d8519400266c8d5484f4a5404b4c470'
